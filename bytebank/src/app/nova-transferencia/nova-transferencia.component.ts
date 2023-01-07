d256dc56c5e2338515ad4e7ef434b32d013ee613
Thiago Marçal <thiagomarcal1984@gmail.com>
2023-01-07 16:08:29 -0300

    Property Bind.

--- bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
+  valor: number = 12; // Este valor já aparece no formulário por padrão.
+  destino: number = 34; // Este valor já aparece no formulário por padrão.
+
   transferir() {
     console.log('Solicitada nova transferência.');
+    console.log('Valor: ', this.valor);
+    console.log('Destino: ', this.destino);
   }
 }
