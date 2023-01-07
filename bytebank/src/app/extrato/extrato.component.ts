7ed159c0a68b8837974856f9308fad15570ba4e7
Thiago Marçal <thiagomarcal1984@gmail.com>
2023-01-07 19:09:32 -0300

    Enviando dados para o componente.

--- bytebank/src/app/extrato/extrato.component.ts
+++ bytebank/src/app/extrato/extrato.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-extrato',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./extrato.component.scss']
 })
 export class ExtratoComponent {
-
+  @Input() transferencia: any;
 }
