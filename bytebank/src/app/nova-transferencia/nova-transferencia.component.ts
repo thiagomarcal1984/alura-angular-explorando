973f2b1270a92b29f9bb1becfcee574bf3ea58cc
Thiago Marçal <thiagomarcal1984@gmail.com>
2023-01-07 15:37:28 -0300

    Criando o evento do formulário.

--- bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
-  title = 'Nova Transferência';
+  transferir() {
+    console.log('Solicitada nova transferência.');
+  }
 }
