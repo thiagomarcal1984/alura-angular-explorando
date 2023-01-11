f0b28f10838a05557576dbf7febf23419f7527bb
Thiago Marçal <thiagomarcal1984@gmail.com>
2023-01-11 15:35:38 -0300

    Criando primeiro service.

--- bytebank/src/app/extrato/extrato.component.ts
+++ bytebank/src/app/extrato/extrato.component.ts
@@ -1,3 +1,4 @@
+import { TransferenciaService } from './../services/transferencia.service';
 import { Component, Input } from '@angular/core';
 
 @Component({
@@ -6,5 +7,11 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./extrato.component.scss']
 })
 export class ExtratoComponent {
-  @Input() transferencias: any[];
+  transferencias: any[];
+
+  constructor(private service: TransferenciaService) {}
+
+  ngOnInit() {
+    this.transferencias = this.service.transferencias;
+  }
 }
